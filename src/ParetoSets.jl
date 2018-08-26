cc9286f7bdfb2f98f21e79c47f301069d6f7714d
Ritchie Lee <ritchie.lee@gmail.com>
2018-08-25 22:25:37 -0700

    initial

--- src/ParetoSets.jl
+++ src/ParetoSets.jl
@@ -1,5 +1,54 @@
 module ParetoSets
 
-# package code goes here
+export dominates, naive_pareto
+export ParetoSet, pareto_ids, pareto_ys
+
+"""
+    dominates(y, y´)
+
+Returns true if y dominates y´, false otherwise.
+"""
+dominates(y::Vector{Float64}, y´::Vector{Float64}) = all(y .≤ y´) && any(y .< y´)
+
+"""
+    naive_pareto{T}(ys::AbstractVector{Vector{T}})
+
+Returns the pareto set of a collection of points ys as a vector of indices into ys.  Uses naive O(n^2) algorithm.
+"""
+function naive_pareto(ys::AbstractVector{Vector{Float64}})
+    pareto_ids = Int[] 
+    for (i,y) in enumerate(ys)
+        if !any(dominates(y´,y) for y´ in ys)
+            push!(pareto_ids, i)
+        end
+    end
+    return pareto_ids
+end
+
+mutable struct ParetoSet
+    ys::Vector{Vector{Float64}}  #collection of all points
+    pareto_ids::Vector{Int}  #vector of indices into full containing pareto set
+end
+ParetoSet() = ParetoSet(Vector{Float64}[], Int[])
+
+function Base.push!(p::ParetoSet, y::Vector{Float64})
+    push!(p.ys, y)
+    yi = length(p.ys)
+    if !any(dominates(y´, y) for y´ in p.ys)
+        push!(p.pareto_ids, yi)
+    end
+    ids = find(y´->dominates(y,y´), view(p.ys, p.pareto_ids))
+    deleteat!(p.pareto_ids, ids)
+end
+
+pareto_ids(p::ParetoSet) = p.pareto_ids
+pareto_ys(p::ParetoSet) = view(p.ys, p.pareto_ids)
+
+Base.getindex(p::ParetoSet, i) = p.ys[i]
+function Base.append!(ps::ParetoSet, ys)
+    for y in ys
+        push!(ps, y)
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,40 @@ else
 end
 
 # write your own tests here
-@test 1 == 2
+let
+    x = [0.5, 0.5] 
+    y = [1.0, 2.0] 
+    z = [1.5, 1.5] 
+    
+    @test dominates(x, y)
+    @test !dominates(y, x)
+    @test dominates(x, z)
+    @test !dominates(z, x)
+    @test !dominates(y, z)
+    @test !dominates(z, y)
+end
+
+let
+    x = [0.01, 0.02]
+    y = [0.02, 0.01]
+    v = Vector{Float64}[]
+    push!(v, x)
+    push!(v, y)
+    for i = 1:10
+        push!(v, rand(2) .+ 0.02)
+    end
+    ids = naive_pareto(v)
+    @test ids == [1, 2]
+
+    ps = ParetoSet()
+    push!(ps, v[1])
+    @test pareto_ids(ps) == [1]
+    @test pareto_ys(ps) == [x]
+    push!(ps, v[2])
+    @test pareto_ids(ps) == [1,2]
+    @test pareto_ys(ps) == [x,y]
+    append!(ps, v[3:end])
+    @test pareto_ids(ps) == [1,2]
+    @test pareto_ys(ps) == [x,y]
+    @test pareto_ids(ps) == naive_pareto(v)
+end
